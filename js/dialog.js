6023a6d4eb3b73ef03f386d9e91bbe755288e6da
veeti174 <veeti.isokangas@metropolia.fi>
2025-08-31 22:51:33 +0300

    Initial commit linking local project to TVT-WS25-wk2

---
+++ js/dialog.js
@@ -0,0 +1,12 @@
+const dialog = document.querySelector("dialog");
+const dialog = document.getElementById("dialog");
+const jsCloseBtn = dialog.querySelector("#js-close");
+
+showBtn.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+jsCloseBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  dialog.close();
+});
